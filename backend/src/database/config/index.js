d3a356182aae39453a88e676bfc0f821654a9ebe
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-05-30 21:08:26 +0300

    added the database configuration and connected it

---
+++ backend/src/database/config/index.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+  },
+};
